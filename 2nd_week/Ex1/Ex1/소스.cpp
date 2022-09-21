0116c032014a05d9c27fee5b1c58d5a494cc9586
sovoscom <105912173+sovoscom@users.noreply.github.com>
2022-09-21 10:26:09 +0900

    2주차 1번문제
    main 함수가 필수적으로 있어야한다는 사실을 알게되었습니다.

---
+++ 2nd_week/Ex1/Ex1/소스.cpp
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Student Id : %d \n", 32165);
+	printf("Name : %s \n", "Kim jimin");
+	printf("Grade : %c \n", 'A');
+
+	return 0;
+};

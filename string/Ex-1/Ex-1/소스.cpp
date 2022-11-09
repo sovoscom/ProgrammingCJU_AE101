1061a9084117905446470b3ae9935d2883b250f2
sovoscom <spacesafari@naver.com>
2022-11-09 23:30:38 +0900

    8주차 연습문제 1번
    vs2022로 코드를 했을 때는 strncpy가 책과 다르게 작동하여서 대체자를 못 찾고 실행을 하지 못하였지만, vscode를 사용했을 경우에는 정상적으로 작동이 완료했다.

--- string/Ex-1/Ex-1/소스.cpp
+++ string/Ex-1/Ex-1/소스.cpp
@@ -6,10 +6,9 @@ int main(void)
 	char str[80] = {};
 	char strb[10] = "apple";
 	const char* strc = "pear";
-	strcpy_s(str, sizeof(str), "wine");
-	strcat_s(str, sizeof(strb), strb);
-	printf("%s, %d\n", str, strlen(str));
-	strncpy_s(str, sizeof(str), strc, 1);
+	strcpy(str, "wine");
+	strcat(str, strb);
+	strncpy(str, strc, 1);
 	printf("%s, %d\n", str, strlen(str));
 	return 0;	
 }

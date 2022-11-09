cad6ff06f52ef1e2dfdb254a4946531ca6191649
sovoscom <spacesafari@naver.com>
2022-11-09 23:55:54 +0900

    8주차 2번 문제
    일정 길이의 문자열 이후 오는 모든 문자들을 *로 바꾸는 문제이다. 범위를 크게 3가지로 나누어 볼 수 있다. 문자열이 그대로 나오는 경우, 문자열 일부가 *로 바뀌는 경우, 주어진 문자열의 크기보다 큰 경우이다. 모든 상황에서 if else if else문을 사용하여 예외 처리를 한다. 문자열 뒤에 *를 붙일 때에는 6번째 문자열을 NULL 값으로 변환 후 붙이면 *이 모두 붙지 않고 지정된 범위 len-5만큼만 뒤에 생성이 된다.

--- string/Ex-2/Ex-2/소스.cpp
+++ string/Ex-2/Ex-2/소스.cpp
@@ -7,20 +7,28 @@ int main(void)
 	char str_input[80];
 	const char *str_star = "***************";
 	printf("Enter the word: ");
-	scanf_s("%s", str_input, 15);
-	int len = strlen(str_input);
+	//scanf_s("%s", str_input, 15);
+	scanf("%s", str_input);
+	int len = strlen(str_input); 								//get length of str_input
 
 	if (len < 5)
 	{		
-		strcpy_s(str_output, sizeof(str_output), str_input);
-		printf("Input word:%s, Changed word: %s", str_output);
+		//strcpy_s(str_output, sizeof(str_output), str_input);  //when using visual stdio
+		strcpy(str_output, str_input);							//string copy input to output
+		printf("Input word:%s, Changed word: %s", str_output);  //print all input(under 4 letters)
+	}
+	else if (len > 15) 
+	{
+		printf("Enter the word that under 15 characters");      //exception(over 15words)
 	}
 	else
 	{
-		strcpy_s(str_output, sizeof(str_output), str_input);
-		str_output[5] = '\0';
-		strncat_s(str_output, sizeof(str_output), str_star, len - 5);
-		printf("Input word:%s, Changed word: %s", str_input, str_output);
+		//strcpy_s(str_output, sizeof(str_output), str_input);
+		strcpy(str_output,str_input);
+		str_output[5] = '\0';									//empty 5rd place to change to *
+		//strncat_s(str_output, sizeof(str_output), str_star, len - 5);
+		strncat(str_output, str_star, len - 5);					//add * rest of characters
+		printf("Input word:%s, Changed word: %s\n", str_input, str_output);
 	}
 	
 	return 0;

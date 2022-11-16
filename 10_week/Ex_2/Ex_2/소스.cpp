baf6ea4bba4f627ffdecf93cd6a66ef653634782
sovoscom <spacesafari@naver.com>
2022-11-16 18:58:24 +0900

    연습문제 2번
    책 정보를 기입하고 출력하는 코드이다. 정보를 입력할 때, 문자열과 달리 숫자는 주소연산자 &를 입력해야함을 잊고있었다. 그래서 코드를 실행했을 때 가격을 묻는 부분에서 버벅임이 발생하였고, &를 붙인 이후에는 정상적으로 작동하였다. 받은 구조체의 값을 출력을 할 때에는 간격을 주어서 출력 값이 몰려있지 않게 정렬을 하였다.

--- 10_week/Ex_2/Ex_2/소스.cpp
+++ 10_week/Ex_2/Ex_2/소스.cpp
@@ -4,7 +4,7 @@
 typedef struct 
 {
 	char title[80];
-	char author[20];
+	char author[30];
 	int page;
 	int price;
 } Book ;
@@ -14,20 +14,35 @@ void print_list(Book* info)
 	for (int i = 0; i < 3; i++) //make repeat print 'i'times to print all struct on list 
 	{
 		//print title, author, page, price and turn to next row
-		printf("%20s%15s%10d%10d\n",
-			(info + i)->title, (info + i)->author, (info + i)->page, (info + i)->price); //use -> to give code what to print
+		printf("%20s%20s%10d%10d\n",
+			(info + i)->title, (info + i)->author, 
+			(info + i)->page, (info + i)->price); //use -> to give code what to print
 	}
 }
 
 int main(void)
 {	
 	//make struct list by following order(title, author, page, price)
-	Book list[3] =
+	Book list[3] = {0};
+	
+	for(int i =0; i<3; i++)												//repeat 3 times to fill 3 info on list
 	{
-		{"혼자 공부하는 C언어", "서현우", 636, 24000},
-		{ "불편한 편의점", "김호연", 267, 14000 },
-		{ "수학의 위로", "마이클 프레임", 264, 17000 }
-	};
+		printf("Please enter book information by following orders\n"); 
+		printf("Title : ");
+		scanf("%[^\n]s", list[i].title);								//input struct title
+		getchar();
+		printf("Author : ");
+		scanf("%s[^\n]", list[i].author);
+		getchar();
+		printf("Page : ");
+		scanf("%d[^\n]", &list[i].page);								//input page using '&'
+		getchar();
+		printf("Price : ");
+		scanf("%d[^\n]", &list[i].price);
+		getchar();
+		printf("\n");
+	}
+	
 
 	print_list(list);
 }

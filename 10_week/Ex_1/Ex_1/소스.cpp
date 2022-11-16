358d05ef066f4943737de96a62f6540efc01faa8
sovoscom <spacesafari@naver.com>
2022-11-16 20:15:12 +0900

    연습문제 1
    일정 양수 범위에서 소수를 찾고 소수가 아닌 수들은 X로 소수는 그대로 출력하는 코드이다. 소수를 찾는 방식은 첫 소수로 입력된 수까지 나누어보고 다음 소수로 입력된 수까지 나누어보는 방식으로 소수가 아닌 수들은 0으로 만든 후 뒤에 출력 과정에서 if문을 활용하여 X를 출력하게 만들었다.

--- 10_week/Ex_1/Ex_1/소스.cpp
+++ 10_week/Ex_1/Ex_1/소스.cpp
@@ -9,43 +9,49 @@ int main(void)
 	char x = 'X';
 
 	printf("Enter positive number : ");
-	scanf_s("%d", &getNumber);
+	scanf("%d", &getNumber);
 	ptrprimeNumber = (int*)malloc((getNumber - 1) * sizeof(int));
 
-	if (ptrprimeNumber == NULL)
+	if (ptrprimeNumber == NULL)										//get exception for over heap allocation
 	{
 		printf("lack of memories");
+		exit(1);
 	}
-	for (i = 0; i < (getNumber - 1); i++)
+	for (i = 0; i < (getNumber - 1); i++)							//put number on array using index number
 	{
-		ptrprimeNumber[i] = i+2;
+		ptrprimeNumber[i] = i+2;									//prime numbers don't include 1 starting 2
 	}
 	i = 0;
 	while (1)
 	{
-		while ((ptrprimeNumber[i] == 0) && (i < (getNumber - 2))) i++;
-
-		if (i == (getNumber - 2)) break;
-
-		for (int j = i + 1; j < getNumber - 1; j++)
+		while ((ptrprimeNumber[i] == 0) && (i < (getNumber - 2)))	//if integer i reach number which become 0, pass the number 
 		{
-			if ((ptrprimeNumber[j] % ptrprimeNumber[i]) == 0) ptrprimeNumber[j] == 0;
+			 i++;
 		}
-		i++;
+		if (i == (getNumber - 2)) break;							//break infinite while loop when reach last number
+
+		for (int j = i + 1; j < getNumber - 1; j++)					//this number will divide by all of number on i
+		{
+			if ((ptrprimeNumber[j] % ptrprimeNumber[i]) == 0)		//if number can be divide by into number that on i
+			{ 
+				ptrprimeNumber[j] = 0;								//index j number become 0
+			}
+		}
+		i++;														//after for loop i get +1
 	}
 	for (i = 0; i < (getNumber - 2); i++)
 	{
-		if (ptrprimeNumber[i] != 0)
+		if (ptrprimeNumber[i] != 0)									//if index i number stay at the number
 		{
-			printf("%5d", ptrprimeNumber[i]);
+			printf("%5d", ptrprimeNumber[i]);						//print itself
 		}
 		else
 		{
-			printf("%5c", x);
+			printf("%5c", x);										//doesn't
 		}
-		if ((i + 5) % 5 == 0) printf("\n");
+		if ((i + 1) % 5 == 0) printf("\n");							//print X
 	}
-	free(ptrprimeNumber);
+	free(ptrprimeNumber);											//free malloc number
 
 	return 0;
 
